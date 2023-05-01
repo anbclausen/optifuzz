cf98eede0059d3a44dffd57c220df4a3fb1c6d60
Anders Benjamin Clausen <anbclausen@gmail.com>
2023-05-02 00:00:44 +0200

    now piping asm directly from compiler + more generic

--- analysis/assembly_inspection.py
+++ analysis/assembly_inspection.py
@@ -1,34 +1,23 @@
 import os
 import re 
-import time
 
-jmp_regex = re.compile(r"j[a-z]+")
+jmp_regex = re.compile(r"\t(j[a-z]+)")
 compiler = "gcc"
+optimization_flags = ["O0", "O1", "O2", "O3"]
 
 
 def analyze(file):
-    os.popen(f"{compiler} {file} -O0 -c -o {file}_O0.o > /dev/null 2>&1")
-    os.popen(f"{compiler} {file} -O1 -c -o {file}_O1.o > /dev/null 2>&1")
-    os.popen(f"{compiler} {file} -O2 -c -o {file}_O2.o > /dev/null 2>&1")
-    os.popen(f"{compiler} {file} -O3 -c -o {file}_O3.o > /dev/null 2>&1")
-    time.sleep(0.1)
-    o0out = os.popen(f"objdump -d {file}_O0.o").read()
-    o1out = os.popen(f"objdump -d {file}_O1.o").read()
-    o2out = os.popen(f"objdump -d {file}_O2.o").read()
-    o3out = os.popen(f"objdump -d {file}_O3.o").read()
-    o0out = jmp_regex.findall(o0out)
-    o1out = jmp_regex.findall(o1out)
-    o2out = jmp_regex.findall(o2out)
-    o3out = jmp_regex.findall(o3out)
+    out = {}
+    for opt_flag in optimization_flags:
+        out[opt_flag] = os.popen(f"{compiler} {file} -{opt_flag} -S -w -c -o /dev/stdout").read()
     
-    if len(o0out) != len(o1out) or len(o0out) != len(o2out) or len(o0out) != len(o3out):
-        print("### FOUND BRANCHING ###")
-        print(f"File: {file}")
-        print(f"O0 instructions: {str(o0out)}")
-        print(f"O1 instructions: {str(o1out)}")
-        print(f"O2 instructions: {str(o2out)}")
-        print(f"O3 instructions: {str(o3out)}")
-        print("########################")
+    for k, v in out.items():
+        out[k] = jmp_regex.findall(v)
+    
+    if not (out["O0"] == out["O1"] == out["O2"] == out["O3"]):
+        print(f"> File: {file}")
+        for k, v in out.items():
+            print(f"  {k}: {v}")
 
 print("Analyzing assembly instructions...")
 print(f"COMPILER: {compiler}")
