5000b79c421cec324c763c5823766ef2328e31c8
Johan Degn <degnjohan@gmail.com>
2023-05-02 13:16:30 +0200

    make analyze script callable from all locations

--- analysis/assembly_inspection.py
+++ analysis/assembly_inspection.py
@@ -10,7 +10,7 @@ def analyze(file):
     out = {}
     for opt_flag in optimization_flags:
         out[opt_flag] = os.popen(f"{compiler} {file} -{opt_flag} -S -w -c -o /dev/stdout").read()
-    
+
     for k, v in out.items():
         out[k] = jmp_regex.findall(v)
     
@@ -21,7 +21,10 @@ def analyze(file):
 
 print("Analyzing assembly instructions...")
 print(f"COMPILER: {compiler}")
-for dirpath, dnames, fnames in os.walk("programs"):
+
+folder = os.path.dirname(os.path.realpath(__file__))
+programs = f"{folder}{os.sep}programs"
+for dirpath, dnames, fnames in os.walk(programs):
     for f in fnames:
         if f.endswith(".c"):
             analyze(os.path.join(dirpath, f))
