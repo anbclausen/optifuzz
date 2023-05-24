ea6d9fb745533ca691e49696f184b61d1ad3f5bd
Anders Benjamin Clausen <anbclausen@gmail.com>
2023-05-24 13:10:15 +0200

    refactor: inspection, also documented code + removed O0 from opt flags

--- analysis/assembly_inspection.py
+++ analysis/assembly_inspection.py
@@ -4,10 +4,13 @@ import re
 import subprocess
 import json
 
-config = json.load(open("../config.json"))
+CONFIG_FILENAME = "config.json"
 
 folder = os.path.dirname(os.path.realpath(__file__))
-flagged_folder = f"{folder}{os.sep}flagged{os.sep}"
+config_dir = parent_directory = os.path.dirname(folder)
+config = json.load(open(f"{config_dir}{os.sep}{CONFIG_FILENAME}"))
+
+flagged_folder = f"{folder}{os.sep}flagged{os.sep}" # FIXME
 
 # Match all jcc instructions that is not jmp, and all loop instructions
 jmp_regex = re.compile(r"\t(j(?!mp)[a-z][a-z]*)|(loop[a-z]*) ")
@@ -15,39 +18,111 @@ jmp_regex = re.compile(r"\t(j(?!mp)[a-z][a-z]*)|(loop[a-z]*) ")
 optimization_flags = config["compiler_flags"]
 compiler = config["compiler"]
 
-def extract_conditional_branching_instructions(s):
+
+def extract_conditional_branching_instructions(s: str) -> list[str]:
+    """Extracts all conditional branching instructions from a string
+
+    Parameters
+    ----------
+    path : str
+        The string to be searched
+
+    Returns
+    ----------
+    A list of all conditional branching instructions
+    """
     matches = jmp_regex.findall(s)
     jmp_matches = [x[0] for x in matches if x[0] != ""]
     loop_matches = [x[1] for x in matches if x[1] != ""]
     return jmp_matches + loop_matches
 
-def run(args):
+
+def run(args: list[str]) -> str:
+    """Runs a command and returns the stdout
+
+    Parameters
+    ----------
+    args : list[str]
+        The command to be run
+
+    Returns
+    ----------
+    The stdout of the command
+    """
     return subprocess.run(args, capture_output=True, text=True).stdout
 
 
-def flag_file(file):
+def flag_file(file: str) -> None:
+    """Moves a file to the flagged folder
+
+    Parameters
+    ----------
+    file : str
+        The path of the file to be moved
+    """
     os.makedirs(flagged_folder, exist_ok=True)
     run(["mv", file, flagged_folder])
 
 
-def analyze(file):
-    out = {}
-    for opt_flag in optimization_flags:
-        # -S: compile to assembly
-        # -w: disable warnings
-        # -c: compile to object file
-        out[opt_flag] = run(
+def get_assembly_for_file(file: str) -> dict[str, str]:
+    """Gets the assembly for a file for each optimization flag
+
+    Parameters
+    ----------
+    file : str
+        The path of the file to be analyzed
+
+    Returns
+    ----------
+        A dictionary with the optimization flag as key and the assembly as value
+    """
+    return {
+        opt_flag: run(
             [compiler, file, "-S", f"-{opt_flag}", "-w", "-c", "-o", "/dev/stdout"]
         )
+        for opt_flag in optimization_flags
+    }
 
-    for k, v in out.items():
-        out[k] = extract_conditional_branching_instructions(v)
 
-    # Flag the file if any jumps are reported for any flag (except O0)
-    if [None for k, v in out.items() if k != "O0" and v]:
-        flag_file(file)
+def get_cond_branching_instructions(file: str) -> dict[str, list[str]]:
+    """Gets the conditional branching instructions for a file for each optimization flag
+
+    Parameters
+    ----------
+    file : str
+        The path of the file to be analyzed
+
+    Returns
+    ----------
+        A dictionary with the optimization flag as key and lists of conditional branching instructions as value
+    """
+    return {
+        opt_flag: extract_conditional_branching_instructions(assembly)
+        for opt_flag, assembly in get_assembly_for_file(file).items()
+    }
+
+
+def analyze(file: str) -> bool:
+    """Analyzes a file for conditional branching instructions
+
+    Parameters
+    ----------
+    file : str
+        The path of the file to be analyzed
+
+    Returns
+    ----------
+        `True` if any conditional branching instructions are found, `False` otherwise
+    """
+    cond_branching_instructions = get_cond_branching_instructions(file)
+
+    opt_flags_with_jumps = [
+        k for k, v in cond_branching_instructions.items() if v
+    ]
+
+    if opt_flags_with_jumps:
         print(f"\n> File: {file}")
-        for k, v in out.items():
+        for k, v in cond_branching_instructions.items():
             print(f"  {k}: {v}")
         return True
     return False
@@ -55,7 +130,7 @@ def analyze(file):
 
 print("Analyzing assembly instructions...")
 print(f"COMPILER: {compiler}")
-programs_folder = f"{folder}{os.sep}..{os.sep}generator{os.sep}generated"
+programs_folder = f"{folder}{os.sep}..{os.sep}generator{os.sep}generated" # FIXME
 file_amount = len(os.listdir(programs_folder))
 count = 1
 for dirpath, dnames, fnames in os.walk(programs_folder):
@@ -63,5 +138,8 @@ for dirpath, dnames, fnames in os.walk(programs_folder):
         if f.endswith(".c"):
             print(f"\rInspecting [{count}/{file_amount}]", end="", flush=True)
             count += 1
-            analyze(os.path.join(dirpath, f))
+            file_path = os.path.join(dirpath, f)
+            if analyze(file_path):
+                flag_file(file_path)
+
 print("\nDone!")
