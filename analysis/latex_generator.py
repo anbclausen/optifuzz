28253c6c14993f11f5bc498d02a8875b37b54edc
Anders Benjamin Clausen <anbclausen@gmail.com>
2023-05-25 14:30:45 +0200

    fix: now looking at tstatistic as the same as moderate threshold in dudect

--- analysis/latex_generator.py
+++ analysis/latex_generator.py
@@ -574,11 +574,11 @@ def gen_plot_asm_fig(
             _, pval = stats.ttest_ind(fixed_csv.min_clocks, uniform_csv.min_clocks, equal_var = False)
             t_test_result = (
                 "\\vspace*{2mm}\\tiny {\color{green}$H_0$ ACCEPTED!" + " p=" + str("{:.3f}".format(pval)) + " }\ \n"
-                if pval > 0.03
+                if pval > 0.05
                 else
                 "\\vspace*{2mm}\\tiny {\color{red}$H_0$ REJECTED!" + " p=" + str("{:.3f}".format(pval)) + " }\ \n"
             )
-            if pval <= 0.03:
+            if pval <= 0.05:
                 vulnerable_programs[compiler_flags[i - 1]] += 1
 
         lstlisting = TexLstlisting(
